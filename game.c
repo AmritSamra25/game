dad9152d95497c7853881b08896d5f247c57af65
”Amrit <amrit.samra@du.edu>
2024-07-08 11:18:57 -0600

    added game

---
+++ game.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "board.h"
+
+int main(int argc, char **argv) {
+    struct Board b;
+    struct Board *b2 = (struct Board *)malloc(sizeof(struct Board));
+
+    printf ("Initial board sizes: %d %d %d %d\n");
+}
