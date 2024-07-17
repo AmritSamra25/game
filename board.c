f49d75d7c175b06b15715380939828fe23d5440b
Greg Holling <greg@holling-co.com>
2024-07-17 16:45:27 -0600

    Added comments to board.c and board.h, changed return type of board_init so it can return a status value

--- board.c
+++ board.c
@@ -1,7 +1,29 @@
 #include <stdlib.h>
 #include "board.h"
 
-void board_init(struct Board* b, int width, int height) {
+
+/* 
+ * Initialize the board.
+ * Parameters:
+ *     b - a pointer to the board to initialize.
+ *     width - the new board width (number of nodes)
+ *     height - the new board height (number of nodes)
+ *
+ * Returns:
+ *     0 if the initialization went OK
+ *    -1 if b is 0.
+ *    -2 if width or height is <= 0.
+ *
+ * Notes:
+ *     If b is uninitialized, the results are undefined.
+ *     width and height must be positive integers.
+ */
+int board_init(struct Board* b, int width, int height) {
+    if (b == 0) return -1;
+    if ((width<0) || (height<0)) return -2;
+
     b->width = width;
     b->height = height;
+
+    return 0;
 }
--- board.h
+++ board.h
@@ -1,9 +1,24 @@
 #ifndef BOARD
 #define BOARD
+/*
+ * Insert comments here about what the Board structure is
+ * used for, and any other general notes about usage or intent
+ */
 struct Board {
-    int width;
-    int height;
+    int width;     // The width of the board (number of nodes)
+    int height;    // The height of the board (number of nodes)
 };
-void board_init(struct Board* b, int width, int height);
+/* 
+ * board_init is used to  (insert details here)
+ * Parameters:
+ *    b - a pointer to the board struct to initialize.
+ *    width - the new board width.
+ *    height - the new board height:
+ * Returns:
+ *    0 if everything is OK
+ *   -1 if b==0
+ *   -2 if width or height is negative.
+ */
+int board_init(struct Board* b, int width, int height);
 
 #endif
